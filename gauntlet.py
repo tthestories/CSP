f9d6a3f14bff3c6e7d79f50e692bc813dd589e08
Jake <82616410+tthestories@users.noreply.github.com>
2025-01-24 08:21:24 -0600

    Update gauntlet.py

--- gauntlet.py
+++ gauntlet.py
@@ -1,3 +1,4 @@
+# # Don't touch the boss code until Vega says you're ready!
 for i in range(5):
     boss.moveLeft(3)
     boss.moveRight()
@@ -5,20 +6,36 @@ for i in range(5):
  
  # Build your Helper items BELOW this line
 helper.setPlayer("car")
-for i in range(3):
-    helper.moveRight()
-    helper.build("rightArrow")
-     
-for i in range(3):
-    helper.moveDown()
-    helper.build("wall")
+helper.moveDown()
+helper.build("pushable")
+helper.moveUp()
+helper.moveRight()
 
-helper.moveDown()
-helper.build("spikes")
-for i in range (3):
-    helper.moveRight()
-    helper.build("rightArrow")
+for i in range(4):
+    helper.build("wall")
+    helper.moveDown()
+
+for i in range(2):
+    helper.moveDown()
     
-helper.build("wall")
-helper.moveDown()
-helper.build("downArrow")
+for i in range(2):
+    helper.build("wall")
+    helper.moveRight()
+
+helper.build("spikes")
+helper.moveUp()
+helper.build("pushable")
+helper.moveUp()
+helper.build("spikes")
+helper.moveLeft()
+helper.build("spikes")
+
+for i in range(3):
+    helper.moveRight()
+
+for i in range(2):
+    helper.moveDown()
+    
+for i in range(5):
+    helper.build("wall")
+    helper.moveUp()
